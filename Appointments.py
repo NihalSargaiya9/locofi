d7df10664413d92fc3e7a865c39c54323bb3b82e
Nihal <nihalsargaiya4@gmail.com>
2020-03-29 12:34:00 +0530

    small typos

--- Appointments.py
+++ Appointments.py
@@ -3,7 +3,7 @@ from Database import mycursor,printCursor,mydb
 
 class Appointments:
 	def listAppointments(self):
-		sql = "select appointments.sno,notes.content,sales_status.action,geofence.geofence_name from appointments JOIN notes on appointments.note_id=notes.sno join sales_status on appointments.sales_status=sales_status.sales_id JOIN geofence on appointments.geo_id = geofence.geofence_id"
+		sql = "select appointments.sno,notes.content,sales_status.action,geofence.geofence_name,appointments.meeting_with from appointments JOIN notes on appointments.note_id=notes.sno join sales_status on appointments.sales_status=sales_status.sales_id JOIN geofence on appointments.geo_id = geofence.geofence_id"
 		mycursor.execute((sql))
 		myresult = mycursor.fetchall()
 		print(myresult)
--- Database.py
+++ Database.py
@@ -4,7 +4,7 @@ def printCursor(cursor):
     for x in cursor:
         print(x)
 
-mydb = pymysql.connect(host="localhost", user="root",password="" ,database="new_locofi")
+mydb = pymysql.connect(host="localhost", user="root",password="" ,database="locofi")
 
 mycursor = mydb.cursor()
 
